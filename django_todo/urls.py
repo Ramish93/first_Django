89bdc2cc94e771be9301ecec8d2da3ccc81d99e0
Ramish93 <ramishshah93@gmail.com>
2021-11-23 13:30:24 +0000

    added full crud functionality for user in dailytask app

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from todo.views import get_todo, add_item
+from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', get_todo, name='get_todo'),
-    path('add', add_item, name='add')
+    path('', views.get_todo, name='get_todo'),
+    path('add', views.add_item, name='add'),
+    path('edit/<item_id>', views.edit_item, name='edit'),
+    path('toggle/<item_id>', views.toggle_item, name='toggle'),
+    path('delet/<item_id>', views.delet_item, name='delet'),
 ]
